9f3d0a49db4ceb63404f1f85e9a882c67a9359a8
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2015-12-17 17:53:17 -0700

    fix(ESLint): Update JSHint to ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,44 @@
 var webpack = require('webpack');
-
 module.exports = {
-  plugins: [],
-  entry: './lib/index.js',
-  output: {
-    path: './dist',
-    filename: 'tonic-ui.js',
-  },
-  module: {
-    preLoaders: [
-      {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: "jshint!babel?presets[]=react,presets[]=es2015"
-      },{
-          test: /\.js$/,
-          include: /node_modules\/tonic-/,
-          loader: "babel?presets[]=react,presets[]=es2015"
-      },{
-          test: /\.js$/,
-          include: /node_modules\/react-contenteditable/,
-          loader: "babel?presets[]=react,presets[]=es2015"
-      }
-    ],
-    loaders: [
-        {
-          test: /\.css$/,
-          loader: "style-loader!css-loader!autoprefixer-loader?browsers=last 2 version"
-        },{
-          test: require.resolve("./lib/index.js"),
-          loader: "expose?Tonic.ui"
-        },{
-          test: /\.(png|jpg)$/,
-          loader: 'url-loader?limit=8192'
-        },
-    ]
-  },
-  jshint: {
-    esnext: true,
-    browser: true,
-    globalstrict: true // Babel add 'use strict'
-  },
-  externals: {
-    "three": "THREE"
-  }
+    plugins: [],
+    entry: './lib/index.js',
+    output: {
+        path: './dist',
+        filename: 'Tonic-core.js',
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                loader: "eslint-loader",
+                exclude: /node_modules/
+            }
+        ],
+        loaders: [
+            {
+                test: require.resolve("./lib/index.js"),
+                loader: "expose?Tonic.ui"
+            }, {
+                test: /\.css$/,
+                loader: "style-loader!css-loader!autoprefixer-loader?browsers=last 2 version"
+            }, {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=8192'
+            }, {
+                test: /\.js$/,
+                include: /node_modules\/tonic-/,
+                loader: "babel?presets[]=react,presets[]=es2015"
+            }, {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel?presets[]=react,presets[]=es2015"
+            }
+        ]
+    },
+    externals: {
+        "three": "THREE"
+    },
+    eslint: {
+        configFile: '.eslintrc'
+    }
 };
