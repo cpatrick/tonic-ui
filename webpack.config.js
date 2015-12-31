870268ec49c21492e0a8b1cfbc428e5682fef905
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2015-12-30 19:28:51 -0700

    fix(ESLint): Update code formatting to comply with our ESLint specification

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,43 @@
-var webpack = require('webpack');
 module.exports = {
     plugins: [],
     entry: './lib/index.js',
     output: {
         path: './dist',
-        filename: 'Tonic-core.js',
+        filename: 'Tonic-ui.js',
     },
     module: {
         preLoaders: [
             {
                 test: /\.js$/,
                 loader: "eslint-loader",
-                exclude: /node_modules/
-            }
+                exclude: /node_modules/,
+            },
         ],
         loaders: [
             {
                 test: require.resolve("./lib/index.js"),
-                loader: "expose?Tonic.ui"
+                loader: "expose?Tonic.ui",
             }, {
                 test: /\.css$/,
-                loader: "style-loader!css-loader!autoprefixer-loader?browsers=last 2 version"
+                loader: "style-loader!css-loader!autoprefixer-loader?browsers=last 2 version",
             }, {
                 test: /\.(png|jpg)$/,
-                loader: 'url-loader?limit=8192'
+                loader: 'url-loader?limit=8192',
             }, {
                 test: /\.js$/,
                 include: /node_modules\/tonic-/,
-                loader: "babel?presets[]=react,presets[]=es2015"
+                loader: "babel?presets[]=react,presets[]=es2015",
             }, {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel?presets[]=react,presets[]=es2015"
-            }
-        ]
+                loader: "babel?presets[]=react,presets[]=es2015",
+            },
+        ],
     },
     externals: {
-        "three": "THREE"
+        "three": "THREE",
     },
     eslint: {
-        configFile: '.eslintrc'
-    }
+        configFile: '.eslintrc',
+    },
 };
